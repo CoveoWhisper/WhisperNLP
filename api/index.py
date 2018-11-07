07e4989305dfc7f2cd498d32289610bac3bd28dc
xahor <erdnacram15@gmail.com>
2018-11-07 00:06:43 -0500

    using the model

--- api/index.py
+++ api/index.py
@@ -1,8 +1,12 @@
-from flask import Flask,jsonify, request
+from flask import Flask, jsonify, request
 from api.query_parser import QueryParser
 from api.NLPJSONEncoder import NLPJSONEncoder
 from api.dialogflow_service import DialogFlowService
 from api.logger.logger_factory import LoggerFactory
+
+import nltk
+import pickle
+
 app = Flask(__name__)
 app.json_encoder = NLPJSONEncoder
 dialogflowservice = DialogFlowService()
@@ -13,10 +17,15 @@ def nlp_analyze():
     content = request.get_json()
     query = content['sentence']
     nlp_analysis = dialogflowservice.analyse_sentence(query)
-    nlp_analysis.add_query(QueryParser.parse_query(query))
+    nlp_analysis.add_query(query_parser.parse_query(query))
     return jsonify(nlp_analysis)
 
 
 if __name__ == '__main__':
+    word_model_file = open('checkpoint.bin', 'rb')
+    word_model = pickle.load(word_model_file)
+    word_model_file.close()
+    query_parser = QueryParser(word_model)
+    nltk.download('stopwords')
     LoggerFactory.get_logger(__name__).info("API started")
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=50001)
--- api/query_parser.py
+++ api/query_parser.py
@@ -1,5 +1,31 @@
-class QueryParser(object):
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+import re
+import heapq
 
-    @staticmethod
-    def parse_query(query):
-        return query
+LANGUAGE = 'english'
+
+
+class QueryParser(object):
+    def __init__(self, word_model):
+        self.model = word_model
+
+    def parseText(self, text):
+        words = word_tokenize(text, language=LANGUAGE)
+        regex = re.compile('[^a-zA-Z0-9]')
+        words = [regex.sub('', w).lower() for w in words]
+        words = [w for w in words if w]
+        return words
+
+
+    def parse_query(self, sentence):
+        words = self.parseText(sentence)
+        print('Step 1: split query in words ', words)
+        stopWords = set(stopwords.words(LANGUAGE))
+        words = [word for word in words if word not in stopWords]
+        print('Step 2: remove stopwords ', words)
+        word_tuples = [(self.model[w], w) for w in words if w in self.model]
+        heapq.heapify(word_tuples)
+        rarest_words = [w[1] for w in heapq.nsmallest(3, word_tuples)]
+        print('Step 3: keep rarest ', rarest_words)
+        return " ".join(rarest_words)
