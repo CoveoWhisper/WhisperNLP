0d35c964c59da73e2d1910267acfdca204656c8f
EmpireEarth <bruno.allaire-lemay@usherbrooke.ca>
2018-11-29 16:03:24 -0500

    Pretraitement requetes (#16)
    * added new endpoint to parse query
    
    * Now correctly JSONify + all in one endpoint
    
    * using the model
    
    * removed port
    
    * typo
    
    * download punkt
    
    * minor changes
    
    * use the right model and set rarest 5 words
    
    * add_text_data_mining
    
    * add tfidf model
    
    * parse_text_before_applying_to_model
    
    * download nltk resource and added stopwords folder where it should be for deployment
    
    * add_word_and_parsed_word_mapping

---
+++ api/query_parser.py
@@ -0,0 +1,28 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+import math
+from api.text_data_mining import parseText, get_word_mapping
+
+LANGUAGE = 'english'
+
+
+class QueryParser(object):
+    def __init__(self, query_model):
+        self.model = query_model
+
+    def parse_query(self, sentence):
+        parsed_word_by_word = get_word_mapping(sentence)
+
+        tfidf = self.model.transform([parseText(sentence)])
+
+        feature_names = self.model.get_feature_names()
+        scores = []
+        for col in tfidf.nonzero()[1]:
+            new_score = (feature_names[col], math.ceil(tfidf[0, col] * 1000) / 1000)
+            scores.append(new_score)
+
+        word_by_parsed_word = {value: key for (key, value) in parsed_word_by_word.items()}
+        query_word_score = [(word_by_parsed_word[word], score) for (word, score) in scores]
+        query_word_score.extend([(word,0) for (word, parsed_word) in parsed_word_by_word.items() if parsed_word == ''])
+        query_word_score = sorted(query_word_score, key=lambda x: x[1], reverse=True)
+
+        return ' '.join([word for (word, score) in query_word_score])
---
+++ api/text_data_mining.py
@@ -0,0 +1,90 @@
+import pickle
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
+import re
+from collections import Counter
+
+LANGUAGE = 'english'
+
+def parseText(text):
+    text = lower_cases(text)
+    text = remove_noise(text)
+    text = remove_numbers(text)
+    text = remove_punctuation(text)
+    #print(text)
+    text = remove_stop_words(text)
+    #print("remove stop words : %s" % text)
+    text = remove_chatbot_stop_words(text)
+    #print("remove chatbot stop words : %s" % text)
+    text = do_lemmatization(text)
+    #print("Lemmatization : %s" % text)
+    text = do_stemming(text)
+    #print("Stemming: %s" % text)
+    text = remove_custom_stop_words(text)
+    # print("Remove custom stop words : %s" % (text))
+
+    return text
+
+def lower_cases(text):
+    return text.lower()
+def remove_noise(text):
+    words = word_tokenize(text)
+    noise_sub_words = ['www', '/', 'coveo', 'salesforce', '.', '<0x', '@']
+    for index in range(len(words)):
+       for noise_sub_word in noise_sub_words:
+           if noise_sub_word in words[index]:
+                words[index] = ''
+    return ' '.join([word for word in words if word])
+def remove_numbers(text):
+    return re.sub(r'\d+', ' ', text)
+def remove_punctuation(text):
+    return  re.sub(r'\W+|\_', ' ', text)
+def remove_stop_words(text):
+    stop_words = stopwords.words(LANGUAGE)
+    return ' '.join([i for i in word_tokenize(text) if i not in stop_words])
+def remove_chatbot_stop_words(text):
+    fichier = open('stopwords/chatbot_stop_words.txt', 'r')
+    chatbot_stop_words = fichier.read()
+    fichier.close()
+    fichier = open('stopwords/stop_words_with_personal_names.txt', 'r')
+    stop_words = fichier.read()
+    fichier.close()
+    stop_words += chatbot_stop_words
+    return ' '.join([i for i in word_tokenize(text) if i not in stop_words.split()])
+def remove_custom_stop_words(text):
+    custom_stop_words = stopwords.words(LANGUAGE) + ['endif', 'tell', 'coveo', 'salesforce', 'answer', 'question']
+    return ' '.join([i for i in word_tokenize(text) if i not in custom_stop_words])
+def do_stemming(text):
+    stemmer = SnowballStemmer(LANGUAGE)
+    return ' '.join([stemmer.stem(i) for i in word_tokenize(text)])
+def do_lemmatization(text):
+    lemmatizer = WordNetLemmatizer()
+    return ' '.join([lemmatizer.lemmatize(i) for i in word_tokenize(text)])
+
+def get_word_counts(documents_dict):
+    big_text = ''
+    for document_name in documents_dict.keys():
+        big_text += documents_dict[document_name]
+    return Counter(big_text.split())
+def remove_rare_words(words_count, least_counts_to_remove):
+    min_count = 0
+    least_counts_to_remove = (1, 2)
+    for word in words_count:
+        if words_count[word] in least_counts_to_remove:
+            min_count += 1
+    words_to_remove = words_count.most_common()[:-min_count-1:-1]
+    return (words_count - Counter(dict(words_to_remove)))
+def remove_most_common_word(words_count):
+    most_common_word = words_count.most_common(1)
+    return words_count - Counter(dict(most_common_word))
+def get_word_mapping (text):
+    words = {word : parseText(word) for word in text.split()}
+    query_words = {word : parsed_word for (word, parsed_word) in words.items() if
+                   parsed_word or word.isdigit() or word[1:].replace(".", "", 1).isdigit()}
+    unique_query_words = {}
+    for word, parsed_word in query_words.items():
+        if parsed_word not in unique_query_words.values() or parsed_word == '':
+            unique_query_words[word] = parsed_word
+    return unique_query_words
